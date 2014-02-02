aed31dbc36f24f0f1cbeb8accba525a39a15bd54
sdronava <srinath.dv@gmail.com>
2014-02-02 17:33:30 -0500

    Added scoped queries

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
     belongs_to :article
+    validate :article_should_be_published
+    
+    def article_should_be_published
+      errors.add(:article_id, "is not published yet") if article && !article.published?
+    end
 end
--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :email
+  validates_length_of :email, :within => 5..100
+  
   has_one :profile
   has_many :articles, -> { order('published_at DESC, title ASC')},
             :dependent => :nullify
